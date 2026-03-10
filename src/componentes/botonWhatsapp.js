ecbc7fb3f7be504bca6121544015cca613731df6
Alejandro <auxsistemasinv@clinicadelacosta.co>
2026-03-10 16:46:00 -0500

    Actualicé mi portafolio

--- src/componentes/botonWhatsapp.js
+++ src/componentes/botonWhatsapp.js
@@ -5,7 +5,7 @@ import whatsapp from "../imagenes/whatsapp.png";
 const BotonWhatsapp = () => {
   return (
     <div class="container-boton">
-    <a href="https://wa.me/+573043619700?text=Hola%20Esteban%20" target="_blank">
+    <a href="https://wa.me/+573043619700?text=Hola%20Alejandro%20" target="_blank">
         <img class="boton" src={whatsapp} alt=""/>
     </a>
 </div>
