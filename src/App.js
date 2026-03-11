91c974d15e61778f12f90ab49cb740ce4f9f2be1
Alejandro <auxsistemasinv@clinicadelacosta.co>
2026-03-11 08:27:34 -0500

    Corrijo estiloss

--- src/App.js
+++ src/App.js
@@ -4,39 +4,46 @@ import SectionHabilidad from "./componentes/sectionHabilidad.js";
 import SectionProyecto from "./componentes/sectionProyecto.js";
 import Navegacion from "./componentes/navegacion.js";
 import BotonWhatsapp from "./componentes/botonWhatsapp.js";
-
 import SectionExp from "./componentes/sectionExp.js";
 import perfil from "./imagenes/yo.jpeg";
 
 function App() {
   return (
     <>
-      <body>
-        <Navegacion />
-        <section id="section-inicio" class="sect">
-          <div class="custom-div">
-            <div class="contenedor-redondo">
-              <img src={perfil} alt="Foto" className="foto" />
-            </div>
-          </div>
-          <div className="custom-div">
-            <div class="animacion">
-              <h2>DESARROLLADOR WEB</h2>
-            </div>
-            <br />
-            <p className="parrafo">
-              ¡Hola! soy{" "}
-              <span style={{ color: "#5468FF" }}>Alejandro Sampayo</span>, un desarrollador web con un enfoque creativo y orientado a soluciones tecnológicas innovadoras. Mi camino en el mundo del desarrollo comenzó con la curiosidad por explorar nuevas posibilidades y la pasión por crear experiencias digitales impactantes.
-            </p>
-            <br />
+      <Navegacion />
 
-            <BotonDescarga nombre={"Descargar HV"} href={"./H_Alejandro.pdf"} />
+      <section id="section-inicio" className="sect">
+        <div className="custom-div">
+          <div className="contenedor-redondo">
+            <img src={perfil} alt="Foto de Alejandro Sampayo" className="foto" />
           </div>
-        </section>
-        <SectionHabilidad />
-        <SectionProyecto />
-        <SectionExp />
-      </body>
+        </div>
+
+        <div className="custom-div">
+          <div className="animacion">
+            <h2>DESARROLLADOR WEB</h2>
+          </div>
+
+          <br />
+
+          <p className="parrafo">
+            ¡Hola! soy{" "}
+            <span style={{ color: "#5468FF" }}>Alejandro Sampayo</span>, un
+            desarrollador web con un enfoque creativo y orientado a soluciones
+            tecnológicas innovadoras. Mi camino en el mundo del desarrollo
+            comenzó con la curiosidad por explorar nuevas posibilidades y la
+            pasión por crear experiencias digitales impactantes.
+          </p>
+
+          <br />
+
+          <BotonDescarga nombre={"Descargar HV"} href={"./H_Alejandro.pdf"} />
+        </div>
+      </section>
+
+      <SectionHabilidad />
+      <SectionProyecto />
+      <SectionExp />
       <BotonWhatsapp />
     </>
   );
--- src/componentes/navegacion.js
+++ src/componentes/navegacion.js
@@ -1,4 +1,3 @@
-// Navegacion.jsx
 import React, { useState } from "react";
 import "../estilosComponentes/navegacion.css";
 
@@ -14,6 +13,7 @@ const Navegacion = () => {
       <button onClick={toggleMenu} id="menu-toggle" className="menu-button">
         ☰
       </button>
+
       <nav className={`navbar ${menuVisible ? "show-menu" : ""}`}>
         <a href="#section-inicio" className="nav-item">
           Inicio
@@ -24,15 +24,8 @@ const Navegacion = () => {
         <a href="#section-proyecto" className="nav-item">
           Proyectos
         </a>
-
         <a href="#section-exp" className="nav-item">
           Experiencia
-        </a>
-        <a href="#" className="nav-item">
-          Estudios
-        </a>
-        <a href="#" className="nav-item">
-          Contacto
         </a>
       </nav>
     </>
